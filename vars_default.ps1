b47bc66d97d6ca25ddb03b7894e8d033245336df
Paul Broadwith <paul@pauby.com>
2018-10-15 19:46:47 +0100

    Removed

--- vars_default.ps1
+++
@@ -1,6 +0,0 @@
-# Edit variable values and then save this file as 'vars.ps1' to get it included into the publish procedure.
-
-$Env:Github_UserRepo   = ''   # Publish to Github; commit git changes
-$Env:Github_ApiKey     = ''   # Publish to Github token
-$Env:NuGet_ApiKey      = ''   # Publish to PSGallery token
-$Env:Chocolatey_ApiKey = ''   # Publish to Chocolatey token
